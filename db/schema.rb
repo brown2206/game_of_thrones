bc8e8ce5754ce8cfdab1a49284ccbb97c5e9d359
brown2206 <brown.angelo.jr@gmail.com>
2016-10-13 10:47:40 -0400

    successfully tested rails in console

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20161013134208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "characters", force: :cascade do |t|
+    t.string  "name"
+    t.string  "img_url"
+    t.integer "house_id"
+    t.index ["house_id"], name: "index_characters_on_house_id", using: :btree
+  end
+
+  create_table "houses", force: :cascade do |t|
+    t.string "name"
+    t.string "img_url"
+  end
+
+  add_foreign_key "characters", "houses"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+House.destroy_all
+Character.destroy_all
+
+House.create!([
+  {name: "Lannister",img_url: "http://vignette3.wikia.nocookie.net/gameofthrones/images/8/8a/House-Lannister-Main-Shield.PNG/revision/latest?cb=20151207184048 "}
+  ])
+
+instance = House.first
+instance.characters.create(name: "Tyrion", img_url: "http://vignette4.wikia.nocookie.net/gameofthrones/images/9/9d/Tyrion6x082.png/revision/latest?cb=20160609061404")
